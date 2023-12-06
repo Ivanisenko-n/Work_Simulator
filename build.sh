aa6b9060c3f12a080a1222618574fd4c5bded8ef
nikolay <spam@alg-web.ru>
2023-12-06 14:37:56 +0300

    add license and build

---
+++ build.sh
@@ -0,0 +1,2 @@
+pyinstaller --onefile main.py
+create-dmg dist/main.app --overwrite --dmg-title="Work Simulator" --overwrite --dmg-props="title=Work Simulator" --license="path/to/license.txt" --overwrite
