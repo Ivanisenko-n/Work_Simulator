d1511db3ef56125ede5fec65c6e4a03e3f31e1df
nikolay <spam@alg-web.ru>
2023-12-06 15:01:06 +0300

    new dmg sh script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 pyinstaller --onefile main.py
-create-dmg main --overwrite --dmg-title="Work Simulator" --overwrite --dmg-props="title=Work Simulator" --license="path/to/license.txt" --overwrite
+dmgbuild -s dmgbuild_settings.py
